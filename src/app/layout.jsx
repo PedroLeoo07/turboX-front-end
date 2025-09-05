02c38da22dd77896cfae407063a33cc29fb91cc2
Leonardo Pedro <leonardodeoliveira.tds1.senai@gmail.com>
2025-09-05 15:16:46 -0300

    feat: Update font family to Montserrat and Anton across multiple CSS files for improved typography

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,6 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html>
+            <head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+                <link href="https://fonts.googleapis.com/css2?family=Anton&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+            </head>
             <body>
                 {children}
                 <ToastContainer
